a2e9d40f4bebc6ee55b08af417213273547a4370
Netz <49691921+Netz00@users.noreply.github.com>
2021-08-05 22:28:32 +0200

    1.3

--- src/contracts/contracts/myERC20.sol
+++ src/contracts/contracts/myERC20.sol
@@ -5,16 +5,13 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract myERC20 is ERC20 {
-    constructor(string memory _name, string memory _symbol)
-        public
-        ERC20(_name, _symbol)
-    {}
-
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol){}
     function mint(address account, uint256 amount) public {
         _mint(account, amount);
     }
-
     function burn(address account, uint256 amount) public {
         _burn(account, amount);
     }
 }
+
+//    constructor(string memory _name, string memory _symbol) public ERC20(_name, _symbol){}
